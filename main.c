4ab9c990829a796186e328d48cf8f0a5f52c5bc3
lerosua <lerosua@gmail.com>
2009-09-23 18:46:15 +0800

    test main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2009年09月23日 18时44分21秒 CST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  lerosua (), lerosua@gmail.com
+ *        Company:  Cyclone
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+  printf("hello world\n");
+
+	    return 0;
+}
