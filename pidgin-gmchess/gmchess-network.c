3f7fd0f2e43617b97207c33cc540b6516b90c24b
lerosua <lerosua@gmail.com>
2010-04-08 16:06:34 +0800

    添加gmchess按钮到pidgin窗口

--- pidgin-gmchess/gmchess-network.c
+++ pidgin-gmchess/gmchess-network.c
@@ -38,21 +38,103 @@ writing_im_msg_cb(PurpleAccount *account, const char *who, char **buffer,
 	}
 }
 
+static void
+gmchess_button_cb(GtkButton* button, PidginConversation* gtkconv)
+{
+
+	gchar* joinstr;
+	joinstr = g_strdup_printf("can you play gmchess with me?");
+    gtk_imhtml_append_text(GTK_IMHTML(gtkconv->entry), joinstr, FALSE);
+    g_signal_emit_by_name(gtkconv->entry, "message_send");
+	g_free(joinstr);
+}
+
+static void
+create_gmchess_button_pidgin(PidginConversation* conv)
+{
+    GtkWidget *button, *image, *sep;
+    //GtkIconInfo *icoinfo;
+    //const gchar *iconames[]={"gmchess-pidgin", "gmchess-pidgin"};
+    button = gtk_button_new();
+    gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
+    g_signal_connect(G_OBJECT(button), "clicked",
+	G_CALLBACK(gmchess_button_cb), conv);
+#ifdef DATADIR
+    //image = gtk_image_new_from_file(g_build_filename(DATADIR,"pixmaps","hicolor","22x22","apps","gmchess.png",NULL));
+    //image = gtk_image_new_from_file(g_build_filename("/usr/share","icons","hicolor","22x22","apps","gmchess.png",NULL));
+    image = gtk_image_new_from_file("/usr/share/icons/hicolor/22x22/apps/gmchess.png");
+#else
+	//image = gtk_image_new_from_file(g_build_filename("/usr/share","icons","hicolor","22x22","apps","gmchess.png",NULL));
+    image = gtk_image_new_from_file("/usr/share/icons/hicolor/22x22/apps/gmchess.png");
+#endif
+    gtk_container_add((GtkContainer *)button, image);
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_widget_show(image);
+    gtk_widget_show(button);
+    gtk_box_pack_start(GTK_BOX(conv->toolbar), sep, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(conv->toolbar), button, FALSE, FALSE, 0);
+    g_object_set_data(G_OBJECT(conv->toolbar), "gmchess_separator", sep);
+    g_object_set_data(G_OBJECT(conv->toolbar), "gmchess_button", button);
+
+}
+
+static void
+remove_gmchess_button_pidgin(PidginConversation* gtkconv)
+{
+
+    GtkWidget *gmchess_button = NULL, *separator = NULL;
+
+    gmchess_button = g_object_get_data(G_OBJECT(gtkconv->toolbar), "gmchess_button");
+    if (gmchess_button != NULL) {
+        gtk_container_remove(GTK_CONTAINER(gtkconv->toolbar), gmchess_button);
+        g_signal_handlers_disconnect_by_func(G_OBJECT(gmchess_button),
+                G_CALLBACK(gmchess_button_cb), gtkconv);
+        gtk_widget_destroy(gmchess_button);
+    }
+
+    separator = g_object_get_data(G_OBJECT(gtkconv->toolbar), "gmchess_separator");
+    if (gmchess_button != NULL) {
+        gtk_container_remove(GTK_CONTAINER(gtkconv->toolbar), separator);
+        gtk_widget_destroy(separator);
+    }
+}
 static gboolean
 plugin_load(PurplePlugin *plugin)
 {
+	GList *convs = purple_get_conversations();
 	void *conv_handle = purple_conversations_get_handle();
+	void* gtk_conv_handle = pidgin_conversations_get_handle();
 	
-	purple_debug(PURPLE_DEBUG_INFO, "plugins", "gmchess support plugin loaded.\n");
+	purple_signal_connect(gtk_conv_handle,"conversation-displayed",plugin,PURPLE_CALLBACK(create_gmchess_button_pidgin),NULL);
+	while(convs){
+		PurpleConversation *conv = (PurpleConversation *)convs->data;
+		/* Setup gmchess button */
+		if (PIDGIN_IS_PIDGIN_CONVERSATION(conv)) {
+		    create_gmchess_button_pidgin(PIDGIN_CONVERSATION(conv));
+		}
+
+		convs = convs->next;
+	}
 
 	purple_signal_connect(conv_handle, "writing-im-msg", plugin, PURPLE_CALLBACK(writing_im_msg_cb), NULL);	
 
+	purple_debug(PURPLE_DEBUG_INFO, "plugins", "gmchess support plugin loaded.\n");
 	return TRUE;
 }
 
 static gboolean
 plugin_unload(PurplePlugin *plugin)
 {
+	GList *convs=purple_get_conversations();
+    while (convs) {
+        PurpleConversation *conv = (PurpleConversation *)convs->data;
+
+	if(PIDGIN_IS_PIDGIN_CONVERSATION(conv)){
+		remove_gmchess_button_pidgin(PIDGIN_CONVERSATION(conv));
+	}
+	convs = convs->next;
+    }
 	purple_debug(PURPLE_DEBUG_INFO, "plugins", "gmchess support plugin unloaded.\n");
 
 	return TRUE;
