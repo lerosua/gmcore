7e316135364936b5deeda655358c9ad1180d3e48
lerosua <huangchunhua@redflag-linux.com>
2010-07-30 16:50:39 +0800

    add evmaker project

--- main.c
+++ main.c
@@ -17,11 +17,25 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define NUM 1024
 int main(int argc,char *argv[])
 {
 	int foo = 437;
-	  printf("hello world\n");
+	printf("hello world\n");
+
+	char buf[NUM];
+	memset(buf, '1',NUM);
+	char var[NUM]="test for linux";
+
+	char dbuf[NUM];
+	strncpy(dbuf, var, strlen(var));
+	strncpy(buf, var, strlen(var));
+	buf[strlen(var)] = 0;
+
+	printf("buf = %s, var= %s, dbuf = %s\n",buf, var,dbuf);
 
 	    return 0;
 }
