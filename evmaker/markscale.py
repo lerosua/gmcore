df69166acd8e47bd0bad256b1e7ab073765d744c
lerosua <huangchunhua@redflag-linux.com>
2010-08-04 13:47:07 +0800

    fix gtkmarkscale

--- evmaker/markscale.py
+++ evmaker/markscale.py
@@ -3,21 +3,17 @@
 
 import gtk
 
-class MarkScale(gtk.Adjustment):
-#class MarkScale(gtk.DrawingArea):
+class MarkScale(gtk.HScale):
 
-    def __init__(self, parent):
-        #self.par = parent
-        gtk.Adjustment.__init__(self)
-        #gtk.DrawingArea.__init__(self)
-        #super(MarkScale, self).__init__()
+    def __init__(self, adj):
+        gtk.HScale.__init__(self,adj)
 
-        #self.set_events(gtk.gdk.EXPOSURE_MASK)
+        self.set_events(gtk.gdk.EXPOSURE_MASK)
         self.markA=0
         self.markB=0
         self.nbFrames=0
 
-        self.window.connect("expose-event",self.expose)
+        self.connect("expose-event",self.expose)
 
 
     def setNbFrames(self,total):
@@ -30,28 +26,28 @@ class MarkScale(gtk.Adjustment):
         self.markB=b
 
     def realize(self, widget):
-        #self.parent.realize(widget)
         widget.set_size_request(widget.allocation.width,widget.allocation.height+10)
 
     def expose(self, widget, event):
         if self.nbFrames > 1 :
             gc = widget.window.new_gc()
             width = self.allocation.width
-            start = self.allocation.x + (width-1)*(markA/(nbFrames-1))
-            end   = self.allocation.x + (width-1)*(markB/(nbFrames-1))
+            start = self.allocation.x + (width-1)*(self.markA*1.0/(self.nbFrames-1))
+            end   = self.allocation.x + (width-1)*(self.markB*1.0/(self.nbFrames-1))
             top   = self.allocation.y + 1;
             bottom = self.allocation.y + self.allocation.height - 2
 
+
             widget.window.draw_line(gc, start, top,start+4,top)
-            widget.window.draw_line(gc, start, top,start,  top)
-            widget.window.draw_line(gc, start+4, bottom, start, bottom)
+            widget.window.draw_line(gc, start, top,start,  top+4)
+            widget.window.draw_line(gc, start, bottom, start+4, bottom)
             widget.window.draw_line(gc, start, bottom-4, start, bottom)
 
-            widget.window.draw_line(gc, end, top,end+4,top)
-            widget.window.draw_line(gc, end, top,end,  top)
-            widget.window.draw_line(gc, end+4, bottom, end, bottom)
+            widget.window.draw_line(gc, end, top,end-4,top)
+            widget.window.draw_line(gc, end, top,end,  top+4)
+            widget.window.draw_line(gc, end, bottom, end-4, bottom)
             widget.window.draw_line(gc, end, bottom-4, end, bottom)
 
-            return False
+        return False
                 
 
